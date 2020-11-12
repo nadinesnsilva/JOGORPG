211d39bfcac4174da032a2c86da7bca93ea08972
nadinesnsilva <74214302+nadinesnsilva@users.noreply.github.com>
2020-11-12 17:44:52 -0300

    Add files via upload
    CAMANAPA RPG

---
+++ CAMANAPARPG.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package camanapa.rpg;
+
+/**
+ *
+ * @author Nadine
+ */
+public class CAMANAPARPG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    //@SuppressWarnings("empty-statement")
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here       
+        //System.out.println("1 - InstruÃ§Ãµes");
+            System.out.println("2 - Jogar");
+            System.out.println("3 - CrÃ©ditos");
+            System.out.println("4 - Quer saber mais da histÃ³ria ?");
+            System.out.println("5 - Sair");
+            System.out.print("Escolha uma opÃ§Ã£o: ");
+
+            opcao = entrada.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("VocÃª escolheu a opÃ§Ã£o InstruÃ§Ãµes: ");
+
+                    System.out.println("Escolha JOGAR para iniciar ou HISTÃ“RIA para ler a histÃ³ria do mundo de CAMANAPA.\n"
+                            + "O jogo consiste em 7 desafios baseados na matÃ©ria Conceitos de ComputaÃ§Ã£o, ou seja, vocÃª terÃ¡ de responder perguntas envolvendo conversÃ£o de base, mapa de Karnaugh etc.\n"
+                            + "Ao acertar, pressione ENTER para continuar. Ao errar, pressione ENTER para tentar novamente.\n"
+                            + "VocÃª possuirÃ¡ 3 vidas, logo, poderÃ¡ errar 3 vezes durante o jogo. Caso vocÃª perca todas as vidas, voltarÃ¡ ao inÃ­cio.\n"
+                            + "Acerte todas as perguntas com o mÃ­nimo de erros para vencer! Boa sorte!");
+                    break;
+                case 2:
+                    System.out.println("VocÃª escolheu a opÃ§Ã£o Jogar");
+                    break;
+
+                case 3:
+                    System.out.println("VocÃª escolheu a opÃ§Ã£o CrÃ©ditos");
+
+                    System.out.println("O jogo foi criado e desenvolvidos por: Maria Carolina, Nadine Silva, Paola Soares, Rafael Camilo;");
+                    break;
+                case 4:
+                    System.out.println("VocÃª escolheu a opÃ§Ã£o saber mais da histÃ³ria");
+                    System.out.println("HÃ¡ muito tempo, em uma galÃ¡xia distante... Existia a terra de Camanapa."
+                            + "Seu nome se deu em homenagem a seus guardiÃµes e desenvolvedores, conhecidos por suas iniciais Ca, Ma, Na e Pa, possuindo, respectivamente, os poderes do Ar, da Ã�gua, do Fogo e da Terra.\n"
+                            + "Unidos, possuÃ­am tambÃ©m poderes tecnolÃ³gicos para que Camanapa estivesse em constante evoluÃ§Ã£o. Todos que ali habitavam aproveitavam o equilÃ­brio entre natureza e tecnologÃ­a, e passavam suas vidas buscando conhecimento para que esse equilÃ­brio fosse mantido e aprimorado.\n"
+                            + "PorÃ©m, lideranÃ§as de diferentes partes do planeta buscavam tomar esse poder das mÃ£os dos guardiÃµes originais e estabelecer uma nova ordem. Iniciou-se assim a Grande Guerra.\n"
+                            + "Liderados pelo grande Takeo, os mestres das regiÃµes de Java e da ComputaÃ§Ã£o travaram uma disputa contra os guardiÃµes de Camanapa, que por serem em menor nÃºmero, foram derrotados e exilados do planeta.\n"
+                            + "Vagando pelo universo afora, pensando e avaliando soluÃ§Ãµes para tomar o poder de sua terra de volta, os guardiÃµes decidiram que a melhor pessoa para lidar com a situaÃ§Ã£o e derrotar Takeo seria um grande jogador de RPG. Takeo gostava muito de jogos, especialmente RPG, entÃ£o sua competiÃ§Ã£o para decidir o domÃ­nio do planeta girava em torno de um jogo de Conceitos de ComputaÃ§Ã£o.\n"
+                            + "Os guardiÃµes, depois de muita pesquisa, descobriram que os grandes jogadores se encontravam no planeta Terra.\n"
+                            + "E Ã© aÃ­ onde, literalmente, o jogador entra na histÃ³ria.");
+                    break;
+                case 5:
+                    System.out.println("VocÃª escolheu a opÃ§Ã£o Sair");
+                    break;
+                default:
+                    System.out.println("OpÃ§Ã£o invÃ¡lida!");
+            } case 5:
+System.out.println("Um computador Ã© um equipamento capaz de processar com rapidez e seguranÃ§a grande quantidade de informaÃ§Ãµes.");
+system.out.println("Assim, alÃ©m dos componentes de hardware, os computadores necessitam de um conjunto de softwares denominado.");
+system.out.println("a - arquivo de dados");
+system.out.println("b - blocos de disco.");
+system.out.println("c - navegador de internet.");
+system.out.println("d - processador de dados");
+system.out.println("e - sistema operacional");
+break;
+    
